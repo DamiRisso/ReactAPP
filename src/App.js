d033de06cee275e04d47127d0215b23b9085e46a
DamiRisso <69565808+DamiRisso@users.noreply.github.com>
2020-12-08 22:16:36 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './NavBar';
+import NavBar from './Componentes/NavBar';
 
 function App() {
   return (
