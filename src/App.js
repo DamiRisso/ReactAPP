753f7376e4f70e52b860db690a6e55ec0bffa53b
DamiRisso <69565808+DamiRisso@users.noreply.github.com>
2020-12-17 11:01:05 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer'
+import ItemCount from './components/ItemCount';
 
 function App() {
   return (
    <>
    <NavBar />
    <ItemListContainer saludo='Bienvenidos a nuestra Tienda'/>
+   <ItemCount stock='10' initial='1'/>
    </>
   );
 }
